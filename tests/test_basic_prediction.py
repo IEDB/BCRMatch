d70c040ce7e2cf3e38b87e8fe018d1e796915989
Haeuk <hkim@lji.org>
2025-02-19 03:49:57 -0800

    Adding unittest

---
+++ tests/test_basic_prediction.py
@@ -0,0 +1,183 @@
+import datetime
+import unittest
+import tensorflow as tf
+tf.random.set_seed(42)
+
+import os
+import pandas as pd
+import shutil
+import subprocess
+import sys
+parent_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_directory)
+from io import StringIO
+
+
+def find_root_dir(start_dir=None, anchor_files=None):
+    """Find the app root directory by looking for known anchor files."""
+    if start_dir is None:
+        start_dir = os.getcwd()  # Default to the current working directory
+    
+    if anchor_files is None:
+        anchor_files = ['LICENSE']
+    
+    # Normalize to absolute path
+    current_dir = os.path.abspath(start_dir)
+    
+    # Traverse up the directory tree until an anchor file is found or root is reached
+    while current_dir != os.path.dirname(current_dir):
+        for anchor in anchor_files:
+            if os.path.isfile(os.path.join(current_dir, anchor)) or os.path.isdir(os.path.join(current_dir, anchor)):
+                return current_dir  # Return the directory where the anchor file is found
+        current_dir = os.path.dirname(current_dir)  # Move up one level
+    
+    # If no anchor file is found, return None or raise an error
+    return None
+
+
+def delete_folders_with_prefix(root_dir, prefix="9999"):
+    """Recursively delete folders that start with the specified prefix."""
+    for dirpath, dirnames, filenames in os.walk(root_dir, topdown=False):  # topdown=False ensures we remove subfolders first
+        for dirname in dirnames:
+            if dirname.startswith(prefix):  # Check if the folder starts with the prefix
+                folder_to_remove = os.path.join(dirpath, dirname)
+                print(f"Deleting folder: {folder_to_remove}")
+                shutil.rmtree(folder_to_remove)  # Remove the folder and its contents
+
+
+class TestBasicPrediction(unittest.TestCase):
+    dataset = 'abpairs_abligity_imgt_20240916_hk'
+    python_path = '/usr/bin/python3'
+    example_dir = 'tests/examples'
+    output_file_to_delete = ''  
+
+    def test_abpairs_abligity(self):
+        today = datetime.date.today().strftime('%Y%m%d')
+        training_version = f"9999{today}"
+        output_file = "tests/test_output.csv"
+        ground_truth_file = 'tests/ground_truth_abpair_abligity.csv'
+        self.output_file_to_delete = output_file
+
+        # Training
+        command = [
+            self.python_path, 
+            "run_bcrmatch_new.py", 
+            "-tm", 
+            "-tc", f"{self.dataset}.csv", 
+            # 9999 to indicate that it's a test
+            "-tv", training_version,
+            "-f",
+        ]
+
+        # Call the program and capture output (stdout and stderr)
+        result = subprocess.run(command, capture_output=True, text=True)
+
+        # Check the result
+        if result.returncode == 0:
+            print("Program executed successfully")
+            print("Output:", result.stdout)
+        else:
+            print("Error occurred")
+            print("Error:", result.stderr)
+
+        # Prediction
+        command = [
+            self.python_path,  # the Python executable
+            "run_bcrmatch_new.py",  # the script to run
+            "-ch", f"{self.example_dir}/cdrh1_input.fasta", f"{self.example_dir}/cdrh2_input.fasta", f"{self.example_dir}/cdrh3_input.fasta",  # cdrh arguments
+            "-cl", f"{self.example_dir}/cdrl1_input.fasta", f"{self.example_dir}/cdrl2_input.fasta", f"{self.example_dir}/cdrl3_input.fasta",  # cdrl arguments
+            "-tn", self.dataset,  # training name
+            "-tv", training_version,  # training version
+            "-v",  # verbosity flag
+            "-o", output_file,
+        ]
+
+        result = subprocess.run(command, capture_output=True, text=True)
+        # Check the result
+        if result.returncode == 0:
+            print("Program executed successfully")
+        else:
+            print("Error occurred")
+            print("Error:", result.stderr)
+
+        # Prediction Result
+        df = pd.read_csv(output_file)
+        row = df[df['Antibody pair'] == '24_764']
+
+        # Precalculated Result (Ground Truth)
+        gt_df = pd.read_csv(ground_truth_file)
+        gt_row = gt_df[gt_df['Antibody pair'] == '24_764']
+
+        # RF
+        rf_score = row['RF Prediction'].values[0]
+        gt_rf_score = gt_row['RF Prediction'].values[0]
+        self.assertTrue(self.compare_values(rf_score, gt_rf_score))
+
+        rf_score = row['RF Percentile Rank'].values[0]
+        gt_rf_score = gt_row['RF Percentile Rank'].values[0]
+        self.assertTrue(self.compare_values(rf_score, gt_rf_score))
+
+        # LR
+        lr_score = row['LR Prediction'].values[0]
+        gt_lr_score = gt_row['LR Prediction'].values[0]
+        self.assertTrue(self.compare_values(lr_score, gt_lr_score))
+        
+        lr_score = row['LR Percentile Rank'].values[0]
+        gt_lr_score = gt_row['LR Percentile Rank'].values[0]
+        self.assertTrue(self.compare_values(lr_score, gt_lr_score))
+        
+        # GNB
+        gnb_score = row['GNB Prediction'].values[0]
+        gt_gnb_score = gt_row['GNB Prediction'].values[0]
+        self.assertTrue(self.compare_values(gnb_score, gt_gnb_score))
+        
+        gnb_score = row['GNB Percentile Rank'].values[0]
+        gt_gnb_score = gt_row['GNB Percentile Rank'].values[0]
+        self.assertTrue(self.compare_values(gnb_score, gt_gnb_score))
+        
+        # XGB
+        xgb_score = row['XGB Prediction'].values[0]
+        gt_xgb_score = gt_row['XGB Prediction'].values[0]
+        self.assertTrue(self.compare_values(xgb_score, gt_xgb_score))
+        
+        xgb_score = row['XGB Percentile Rank'].values[0]
+        gt_xgb_score = gt_row['XGB Percentile Rank'].values[0]
+        self.assertTrue(self.compare_values(xgb_score, gt_xgb_score))
+
+        # FFNN
+        ffnn_score = row['FFNN Prediction'].values[0]
+        gt_ffnn_score = gt_row['FFNN Prediction'].values[0]
+        self.assertTrue(self.compare_values(ffnn_score, gt_ffnn_score))
+        
+        ffnn_score = row['FFNN Percentile Rank'].values[0]
+        gt_ffnn_score = gt_row['FFNN Percentile Rank'].values[0]
+        self.assertTrue(self.compare_values(ffnn_score, gt_ffnn_score))
+
+
+    def compare_values(self, f1, f2):
+        return round(f1, 2) == round(f2, 2)
+    
+
+    def tearDown(self):
+        # Example usage
+        root_dir = find_root_dir()
+        if root_dir:
+            print(f"App root directory is: {root_dir}")
+        else:
+            print("Root directory not found.")
+
+        # Delete all the models folder that has been created
+        delete_folders_with_prefix(f'{root_dir}/models', prefix='9999')
+
+        # Delete the output file that was created
+        if os.path.isfile(self.output_file_to_delete):
+            os.remove(self.output_file_to_delete)
+            print(f'File {self.output_file_to_delete} is removed')
+
+def  main():
+    tester = TestBasicPrediction()
+    tester.basic_prediction_abpairs_abligity()
+
+if __name__=='__main__':
+    unittest.main()
+    # main()
